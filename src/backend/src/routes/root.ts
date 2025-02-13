9447c443a47dcd2fb538ce3672e8043db138476f
Mingxi Zhang <mingxi.zhang@curtin.edu.au>
2025-02-13 07:37:10 +0000

    add new pdfs

--- src/backend/src/routes/root.ts
+++ src/backend/src/routes/root.ts
@@ -1,13 +1,42 @@
 import { Readable } from 'node:stream';
 import { type FastifyPluginAsync } from 'fastify';
 
-const root: FastifyPluginAsync = async (fastify, options): Promise<void> => {
+const root: FastifyPluginAsync = async (fastify, _options): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { message: 'server up' };
   });
 
-  // TODO: create /chat endpoint
+  fastify.post('/chat', async function (request, reply) {
+    const { messages } = request.body as any;
+    try {
+      return await fastify.chat.run(messages);
+    } catch (_error: unknown) {
+      const error = _error as Error;
+      fastify.log.error(error);
+      return reply.internalServerError(error.message);
+    }
+  });
+
+  fastify.post('/chat/stream', async function (request, reply) {
+    const { messages } = request.body as any;
+    try {
+      const chunks = createNdJsonStream(await fastify.chat.runWithStreaming(messages));
+      return reply.type('application/x-ndjson').send(Readable.from(chunks));
+    } catch (_error: unknown) {
+      const error = _error as Error;
+      fastify.log.error(error);
+      return reply.internalServerError(error.message);
+    }
+  });
 };
 
-export default root;
+// Transform the response chunks into a JSON stream
+async function* createNdJsonStream(chunks: AsyncGenerator<object>) {
+  for await (const chunk of chunks) {
+    // Format response chunks in Newline delimited JSON
+    // see https://github.com/ndjson/ndjson-spec
+    yield JSON.stringify(chunk) + '\n';
+  }
+}
 
+export default root;
